d0c643e68f3adc7066db29ad5c842425ad33d3fb
Mujahid Aleem <profeshboss@gmail.com>
2020-11-02 17:43:30 -0500

    Adjusted Speaker.java constructor

--- phase1/src/User/Speaker.java
+++ phase1/src/User/Speaker.java
@@ -5,6 +5,7 @@ import Event.Event;
 import java.util.List;
 
 public class Speaker extends User {
+
     public Speaker(String name, String password, List<Event> events, List<User> friends){
         super(name, password, events, friends);
     }

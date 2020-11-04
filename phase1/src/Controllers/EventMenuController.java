a2ec30d9388da734102e40ac5d2519e5ff6a6138
Andrew Zheng <andrewf.zheng@mail.utoronto.ca>
2020-11-04 11:29:34 +0900

    Added the abstract Event Controller and Event Presenter

---
+++ phase1/src/Controllers/EventMenuController.java
@@ -0,0 +1,83 @@
+package src.Controllers;
+
+import java.util.Scanner;
+
+public class EventMenuController {
+    User currentUser;
+    UserManager userManager;
+    EventMenuPresenter presenter;
+    EventManager eventManager;
+
+    public EventMenuController(User currentUser, UserManager userManager,
+                               EventManager eventmanager, EventMenuPresenter presenter) {
+        this.currentUser = currentUser;
+        this.userManager = userManager;
+        this.eventManager = eventmanager;
+        this.presenter = presenter;
+    }
+
+    public void run() {
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            presenter.printMenu(eventManager);
+            String[] command = userInput.nextLine().split(":");
+            if (command[0].equals("return")) {
+                break;
+            }
+            try {
+                switch (command[0]) {
+                    case "Sign up for":
+                        signUpForEvent(findEvent(command[1]));
+                        break;
+                    case "Leave":
+                        removeSpotFromEvent(findEvent(command[1]));
+                        break;
+                    default:
+                        System.out.println("Command does not exist, please try again.");
+                }
+            } catch (NullPointerException e) {
+                System.out.println("Event does not exist, please try again.");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Your command was invalid, please try again.");
+            }
+        }
+    }
+
+    public void signUpForEvent(Event event) {
+        if (userManager.canAttendEvent(currentUser, event) && eventManager.userCanSignUp(currentUser, event)) {
+            userManager.attendEvent(currentUser, event);
+            event.userList.add(currentUser.get_name());
+            presenter.signUpResult(true, event);
+        } else {
+            presenter.signUpResult(false, event);
+        }
+    }
+
+    public void removeSpotFromEvent(Event event) {
+        if (userManager.canLeaveEvent(currentUser, event) && eventManager.userCanLeave(currentUser, event)) {
+            userManager.leaveEvent(currentUser, event);
+            event.userList.remove(currentUser.get_name());
+            presenter.removalResult(true, event);
+        } else {
+            presenter.removalResult(false, event);
+        }
+    }
+
+    protected Event findEvent(String name) {
+        for (Event event : eventManager.events) {
+            if (event.getName().equals(name)) {
+                return event;
+            }
+        }
+        return null;
+    }
+    /**
+     public void displayEventsAttending(){
+
+     }
+
+     public void displayAllEvents(){
+
+     }
+     **/
+}
---
+++ phase1/src/Presenters/EventMenuPresenter.java
@@ -0,0 +1,49 @@
+package src.Presenters;
+
+public class EventMenuPresenter {
+    private User currentUser;
+
+    public EventMenuPresenter(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public void printMenu(EventManager eventManager) {
+        System.out.println("Events Attending");
+        for (Event event : eventManager.events) {
+            if (currentUser.get_personalSchedule().containsKey(event.getName())){
+                System.out.println(event);
+            }
+        }
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("Events Available");
+        for (Event event : eventManager.events) {
+            if (!currentUser.get_personalSchedule().containsKey(event.getName())) {
+                System.out.println(event);
+            }
+        }
+        printCommands();
+    }
+
+    public void signUpResult(boolean i, Event event) {
+        if (i) {
+            System.out.println("You are now registered for " + event.getName() + ".");
+        } else {
+            System.out.println("Sorry, you are unable to attend this event.");
+        }
+    }
+
+    public void removalResult(boolean i, Event event) {
+        if (i) {
+            System.out.println("You are no longer attending " + event + ".");
+        } else {
+            System.out.println("You are already not attending " + event);
+        }
+    }
+
+    public void printCommands(){
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("To sign up for an event, type Sign up for: 'Event'");
+        System.out.println("To cancel your position in an event, type Leave: 'Event'");
+        System.out.println("To return to the main menu, type return");
+    }
+}

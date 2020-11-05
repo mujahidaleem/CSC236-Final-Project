ec96c3fecc61bf5922eb607035cbda1a9d85931a
micha <michaelwu.wu@mail.utoronto.ca>
2020-11-05 12:39:31 -0500

    added event class

--- phase1/src/Entities/Event.java
+++ phase1/src/Entities/Event.java
@@ -1,4 +1,37 @@
 package src.Entities;
+
+import Entities.Organizer;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 //Time, and users are contained in event
 public class Event {
+    String event_name;
+    LocalDateTime event_time;
+    ArrayList<Attendee> attendees;
+    Organizer organizer;
+
+    public Event(String name, LocalDateTime time, Organizer event_organizer){
+        event_name = name;
+        event_time = time;
+        attendees = new ArrayList<Attendee>();
+        organizer = event_organizer;
+    }
+
+    public String getEvent_name(){
+        return event_name;
+    }
+
+    public LocalDateTime getEvent_time(){
+        return event_time;
+    }
+
+    public ArrayList<Attendee> getAttendees(){
+        return attendees;
+    }
+
+    public Organizer getOrganizer(){
+        return organizer;
+    }
 }

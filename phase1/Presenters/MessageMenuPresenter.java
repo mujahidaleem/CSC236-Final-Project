289a669fafd2b5d4624c9cf1c198c6b9f7b8f1d7
wangz389 <zilin.wang@mail.utoronto.ca>
2020-11-15 18:41:49 +0800

    some adjustments

--- phase1/Presenters/MessageMenuPresenter.java
+++ phase1/Presenters/MessageMenuPresenter.java
@@ -11,8 +11,8 @@ public class MessageMenuPresenter {
     public MessageMenuController MessageMenuController;
 
     public MessageMenuPresenter(UserFriendListController UserFriendListController,
-                       UserFriendListPresenter UserFriendListPresenter, User currentUser,
-                       MessageMenuController MessageMenuController) {
+                                UserFriendListPresenter UserFriendListPresenter, User currentUser,
+                                MessageMenuController MessageMenuController) {
         this.UserFriendListPresenter = UserFriendListPresenter;
         this.UserFriendListcontroller = UserFriendListController;
         this.currentUser = currentUser;
@@ -22,17 +22,28 @@ public class MessageMenuPresenter {
 
     public void run() {
         while (true) {
-            this.MessageMenuController.factoryMethod();
+            System.out.println("do you want to return to main menu? (yes ot no)");
+            Scanner userInput0 = new Scanner(System.in);
+            String answer1 = userInput0.nextLine();
+            if (answer1 == "yes") {
+                break;
+            }
+            if (answer1 == "no") {
+                continue;
+            } else {
+                System.out.println("input is invalid");
+            }
+            this.MessageMenuController.factoryMethod(this.currentUser);
             System.out.println("------------------------------------------------------------");
             Scanner userInput = new Scanner(System.in);
             System.out.println("please select a User and type in his/her User name: ");
             String UserName = userInput.nextLine();
             //match username with User
-            User anotherUser=this.MessageMenuController.matchNameWithUser(UserName);
+            User anotherUser = this.MessageMenuController.matchNameWithUser(UserName);
             System.out.println("please select and type in the command number: ");
             System.out.println(("1. send message"));
-            System.out.println("2.add friend");
-            System.out.println("3.remove from friend list");
+            System.out.println("2. add friend");
+            System.out.println("3. remove from friend list");
             Scanner userInput2 = new Scanner((System.in));
             int num = userInput2.nextInt();
             if (num == 1) {
@@ -44,30 +55,33 @@ public class MessageMenuPresenter {
                     this.UserFriendListcontroller.sendingMessage(anotherUser, messageContent);
                     this.UserFriendListPresenter.DisplaySendingMessage(anotherUser);
                     System.out.println("do you want to return to friend list?(yes or no): ");
-                    Scanner answer = new Scanner(System.in);
+                    Scanner userInput3 = new Scanner(System.in);
+                    String answer = userInput3.nextLine();
                     if (answer == "yes") {
                         break;
                     }
                     if (answer == "no") {
                         continue;
+                    } else {
+                        System.out.println("input is invalid");
                     }
+                }}
+                if (num == 2) {
+                    this.UserFriendListcontroller.Add(anotherUser);
+                    this.UserFriendListPresenter.AddMessage(anotherUser);
+                    continue;
                 }
-            }
-            if (num == 2) {
-                this.UserFriendListcontroller.Add(anotherUser);
-                this.UserFriendListPresenter.AddMessage(anotherUser);
-                continue;
-            }
-            if (num == 3) {
-                this.UserFriendListcontroller.removeFrom(anotherUser);
-                this.UserFriendListPresenter.RemoveMessage(anotherUser);
-                continue;
+                if (num == 3) {
+                    this.UserFriendListcontroller.removeFrom(anotherUser);
+                    this.UserFriendListPresenter.RemoveMessage(anotherUser);
+                    continue;
 
+                } else {
+                    System.out.println("input is invalid");
+                }
             }
         }
     }
 
-
-}
 
 

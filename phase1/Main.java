666214cbb3b923af5ed0fd1c834e02992eb36b19
Andrew Zheng <andrewf.zheng@mail.utoronto.ca>
2020-11-16 10:10:35 +0900

    Added the main method, though other things still need to be made for it.

---
+++ phase1/Main.java
@@ -0,0 +1,24 @@
+import Controllers.LoginMenuController;
+import Gateways.UserReader;
+import Gateways.EventReader;
+import Gateways.MessageReader;
+import Presenters.LoginMenuPresenter;
+import UseCases.EventManager;
+import UseCases.UserManager;
+
+public class Main {
+    public static void main(String[] args) {
+        UserReader userReader = new UserReader("D:\\userManager.ser");
+        EventReader eventReader = new EventReader("D:\\eventManager.ser");
+        MessageReader messageReader = new MessageReader(); //TODO input name of file
+
+        UserManager userManager = userReader.readFile();
+        EventManager eventManager = eventReader.readFile();
+        //TODO create messages
+
+        LoginMenuController loginMenuController = new LoginMenuController(userManager);
+        LoginMenuPresenter loginMenuPresenter = new LoginMenuPresenter(loginMenuController);
+        loginMenuPresenter.run();
+        //TODO the presenter should take in the userManager, then create everything else
+    }
+}

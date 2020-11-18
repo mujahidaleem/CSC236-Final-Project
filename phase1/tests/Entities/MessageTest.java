3b7b1c63e554189f60e71d5f1df7c57874fd3a06
Mujahid Aleem <profeshboss@gmail.com>
2020-11-18 16:07:32 -0500

    Added entities skeleton tests

---
+++ phase1/tests/Entities/MessageTest.java
@@ -0,0 +1,20 @@
+package Entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageTest {
+
+    @Test
+    void getSender() {
+    }
+
+    @Test
+    void getRecepient() {
+    }
+
+    @Test
+    void getContent() {
+    }
+}

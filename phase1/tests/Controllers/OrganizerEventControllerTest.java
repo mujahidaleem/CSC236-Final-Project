f04b9719e8bcd83cd78544c0e97751468128d288
Mujahid Aleem <profeshboss@gmail.com>
2020-11-18 16:01:51 -0500

    Added skeleton controller tests

---
+++ phase1/tests/Controllers/OrganizerEventControllerTest.java
@@ -0,0 +1,40 @@
+package Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrganizerEventControllerTest {
+
+    @Test
+    void createEvent() {
+    }
+
+    @Test
+    void assignSpeaker() {
+    }
+
+    @Test
+    void removeSpeaker() {
+    }
+
+    @Test
+    void changeEventDate() {
+    }
+
+    @Test
+    void changeEventRoom() {
+    }
+
+    @Test
+    void deleteEvent() {
+    }
+
+    @Test
+    void eventModifiable() {
+    }
+
+    @Test
+    void createSpeaker() {
+    }
+}

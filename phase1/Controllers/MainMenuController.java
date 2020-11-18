0850150ebfa4e94c6c9f4c1ed0e6a682899f6589
Ruimeng Xu <ruimeng.xu@mail.utoronto.ca>
2020-11-18 00:23:14 -0500

    reformatted

--- phase1/Controllers/MainMenuController.java
+++ phase1/Controllers/MainMenuController.java
@@ -7,6 +7,7 @@ public class MainMenuController {
     private EventMenuPresenter eventMenuPresenter;
     private MessageMenuPresenter messageMenuPresenter;
     private LoginMenuController loginMenuController;
+
     public MainMenuController(EventMenuPresenter eventMenuPresenter, MessageMenuPresenter messageMenuPresenter,
                               LoginMenuController loginMenuController){
         this.eventMenuPresenter = eventMenuPresenter;
@@ -14,11 +15,24 @@ public class MainMenuController {
         this.loginMenuController = loginMenuController;
     }
 
-    public void printEventMenu(){eventMenuPresenter.run();}
-    public void printMessageMenu(){messageMenuPresenter.run();}
-    public void logOut(){loginMenuController.logout();}
-    public void changePw(String password){
-        UserManager.changePassword(password);}
-    public void sysExit(){System.exit(0);}
+    public void printEventMenu(){
+        eventMenuPresenter.run();
     }
+
+    public void printMessageMenu(){
+        messageMenuPresenter.run();
+    }
+
+    public void logOut(){
+        loginMenuController.logout();
+    }
+
+    public void changePw(String password){
+        UserManager.changePassword(password);
+    }
+
+    public void sysExit(){
+        System.exit(0);
+    }
+}
 

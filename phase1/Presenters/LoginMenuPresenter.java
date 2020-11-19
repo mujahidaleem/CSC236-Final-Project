8dd7f15f460f3f01a95208ee53eff151c25d3bd6
Mujahid Aleem <profeshboss@gmail.com>
2020-11-19 17:03:39 -0500

    Debugging: Added some imports of SpeakerFriendListPresenter, fixes to LoginMenuPresenter

--- phase1/Presenters/LoginMenuPresenter.java
+++ phase1/Presenters/LoginMenuPresenter.java
@@ -1,52 +1,38 @@
 package Presenters;
 
 import Controllers.LoginMenuController;
-import UseCases.LoginMenuManager;
-import UseCases.UserManager;
-import Entities.User;
 import java.util.Scanner;
+
+import Controllers.MainMenuController;
 import Presenters.MainMenuPresenter;
 
 
-import sun.lwawt.macosx.CSystemTray;
 
 //TODO:import the various menus for the different types of users.
 
 public class LoginMenuPresenter {
 
-    private static LoginMenuController loginMenuController;
-    private static LoginMenuPresenter mainMenuPresenter;
-    protected LoginMenuManager loginMenuManager;
-    protected LoginMenuController loginMenuController;
-    protected MainMenuPresenter mainMenuPresenter;
+    public LoginMenuController loginMenuController;
 
     /**
      * LoginMenuPresenter constructor
      *
-     * @param loginMenuManager    need to call loginMenuManager
      * @param loginMenuController need to call loginMenuController
      */
 
-    public LoginMenuPresenter(LoginMenuController loginMenuController, LoginMenuManager loginMenuManager) {
-        this.loginMenuController = loginMenuController;
-        this.loginMenuManager = loginMenuManager;
-        this.mainMenuPresenter = mainMenuPresenter;
-    }
-
     public LoginMenuPresenter(LoginMenuController loginMenuController) {
         this.loginMenuController = loginMenuController;
-
-
     }
+
 
     //public void run()
     // figure out how to take multiple command line commands without needing arguments for run()
-    public void run(String[] args) {
+    public void run() {
         Scanner scanner = new Scanner(System.in);
         greetingMessage();
         switch (scanner.next()) {
             case "1":
-                logincommand(scanner.next());
+                loginCommand(scanner.next());
             case "2":
                 System.out.println("Please enter your first and last name");
                 String name = scanner.next();
@@ -56,39 +42,41 @@ public class LoginMenuPresenter {
                 String type = scanner.next();
                 while (type != "attendee" && type != "organizer") {
                     printTryAgain();
-                    type = scanner.next();
+                    String userType = scanner.next();
+            loginMenuController.signUp(name, password, userType);
+            MainMenuPresenter.run();
                 }
         }
 
 
-        loginMenuController.signUp(name, password, type);
         //TODO get the id of this new user, and print "This is your id. please remember it for login purposes"
-        mainMenuPresenter.run();
     }
 
 
-    public static void logincommand(String command) {
+    public void loginCommand(String command) {
         System.out.println("Please enter your id number and password, separated by a space");
         if (loginMenuController.login(command).equals("success")) {
-            mainMenuPresenter.run();
+            MainMenuPresenter.run();
         } else {
             System.out.println("Your username or password is incorrect. Please try again");
-            run();
+            this.run();
 
             }
         }
-    }
+
 
     public static void greetingMessage(){
-                System.out.println("Welcome to the conference! Would you like to log into your existing account or" +
-                         " create a new account?\n" +
-                         "Please enter '1' to log into your existing account, and '2' to create a new account\n" +
-                         "Enter 'exit' at any time to exit the program.");
-            }
+        System.out.println("Welcome to the conference! Would you like to log into your existing account or" +
+                " create a new account?\n" +
+                "Please enter '1' to log into your existing account, and '2' to create a new account\n" +
+                "Enter 'exit' at any time to exit the program.");
+    }
 
     public static void printTryAgain(){
-                System.out.println("Input invalid. Please try again.");
-            }
-
-
+        System.out.println("Input invalid. Please try again.");
+    }
 }
+
+
+
+

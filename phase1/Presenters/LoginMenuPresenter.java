15be18722ba48430105ec7ca5ba6f3e31bc4743e
hannah wen <hannah.wen@mail.utoronto.ca>
2020-11-19 16:09:14 -0800

    edit LoginMenuPresenter

--- phase1/Presenters/LoginMenuPresenter.java
+++ phase1/Presenters/LoginMenuPresenter.java
@@ -1,7 +1,6 @@
 package Presenters;
 
 import Controllers.LoginMenuController;
-import UseCases.LoginMenuManager;
 import UseCases.UserManager;
 import Entities.User;
 import java.util.Scanner;
@@ -16,55 +15,49 @@ public class LoginMenuPresenter {
 
     private static LoginMenuController loginMenuController;
     private static LoginMenuPresenter mainMenuPresenter;
-    protected LoginMenuManager loginMenuManager;
     protected LoginMenuController loginMenuController;
     protected MainMenuPresenter mainMenuPresenter;
 
     /**
      * LoginMenuPresenter constructor
      *
-     * @param loginMenuManager    need to call loginMenuManager
      * @param loginMenuController need to call loginMenuController
      */
 
-    public LoginMenuPresenter(LoginMenuController loginMenuController, LoginMenuManager loginMenuManager) {
+    public LoginMenuPresenter(LoginMenuController loginMenuController,) {
         this.loginMenuController = loginMenuController;
-        this.loginMenuManager = loginMenuManager;
         this.mainMenuPresenter = mainMenuPresenter;
     }
 
     public LoginMenuPresenter(LoginMenuController loginMenuController) {
     }
 
-    //public void run()
-    // figure out how to take multiple command line commands without needing arguments for run()
-    public void run(String[] args) {
+    public void run() {
         Scanner scanner = new Scanner(System.in);
         greetingMessage();
         switch (scanner.next()) {
             case "1":
                 logincommand(scanner.next());
+            case "2":
+                System.out.println("Please enter your first and last name");
+                String name = scanner.next();
+                System.out.println("Please enter your password");
+                String password = scanner.next();
+                System.out.println("Are you an attendee or an organizer?");
+                String type = scanner.next();
+                while (!type.equals("attendee") && !type.equals("organizer")) {
+                    printTryAgain();
+                    type = scanner.next();
+                }
+                loginMenuController.signUp(name, password, type);
+                String id = Integer.parseInt(UserManager.users.length());
+                System.out.println("Your id is" + id + ". Please remember it for the next time you login");
+                mainMenuPresenter.run();
         }
-        case "2":
-        System.out.println("Please enter your first and last name");
-        String name = scanner.next();
-        System.out.println("Please enter your password");
-        String password = scanner.next();
-        System.out.println("Are you an attendee or an organizer?");
-        String type = scanner.next();
-        while (type != "attendee" && type != "organizer") {
-            printTryAgain();
-            type = scanner.next();
-        }
-
-        loginMenuController.signUp(name, password, type);
-        String id = Integer.parseInt(UserManager.users.length());
-        System.out.println("Your id is" + id+". Please remember it for the next time you login");
-        mainMenuPresenter.run();
     }
 
 
-    public static void logincommand(String command) {
+    public void logincommand(String command) {
         System.out.println("Please enter your id number and password, separated by a space");
         if (loginMenuController.login(command).equals("success")) {
             mainMenuPresenter.run();
@@ -74,7 +67,6 @@ public class LoginMenuPresenter {
 
             }
         }
-    }
 
     public static void greetingMessage(){
                 System.out.println("Welcome to the conference! Would you like to log into your existing account or" +

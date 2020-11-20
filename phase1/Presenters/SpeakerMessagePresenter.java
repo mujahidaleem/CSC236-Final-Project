9cd37744d590974de950b920f6e1227922db9c97
Mujahid Aleem <profeshboss@gmail.com>
2020-11-20 03:27:34 -0500

    javadoc and optimizations EVERYWHERE

--- phase1/Presenters/SpeakerMessagePresenter.java
+++ phase1/Presenters/SpeakerMessagePresenter.java
@@ -5,7 +5,14 @@ import UseCases.SpeakerFriendManager;
 import UseCases.UserManager;
 
 public class SpeakerMessagePresenter extends MessageMenuPresenter{
-
+    /**
+     * Constructor of the SpeakerMessagePresenter
+     * Presents the message menu of the speaker this is the same for a MessageMenuPresenter
+     * *Created for extension in phase 2*
+     * @param speakerFriendListController Controller of the speaker's friend list
+     * @param userManager Use case for a user's functions
+     * @param speakerFriendManager Use case for the speaker's friend list's functions
+     */
     public SpeakerMessagePresenter(SpeakerFriendListController speakerFriendListController, UserManager userManager, SpeakerFriendManager speakerFriendManager){
         super(speakerFriendListController,userManager, speakerFriendManager);
     }

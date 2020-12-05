6d4c08c4b42fdca031033189e5564016c98bc9d5
Ruimeng Xu <ruimeng.xu@mail.utoronto.ca>
2020-12-04 20:39:00 -0500

    GUI prototype added

---
+++ phase2/GUI/MainMenuGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class MainMenuGUI {
+    public static void main(){
+        JFrame frame = new JFrame("Main Menu");
+        frame.setSize(1200, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        JPanel mainMenuPanel = new JPanel();
+        frame.add(mainMenuPanel);
+        frame.setVisible(true);
+    }
+}

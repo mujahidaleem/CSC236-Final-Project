e80944ee6ca4c1c2e31b079a3d5dd622715d93e4
hannah wen <hannah.wen@mail.utoronto.ca>
2020-12-10 02:33:15 -0800

    edit MessageMenuGUI, add GUI for sending message function and theme selector

---
+++ phase2/GUI/SendMessageGUI.java
@@ -0,0 +1,9 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SendMessageGUI {
+}
---
+++ phase2/GUI/ThemeSelector.java
@@ -0,0 +1,29 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ThemeSelector {
+    //create buttons with different themes options
+
+    /**
+     * Light theme
+     *     background: (247, 245, 239)
+     *     panels: (195, 226, 230)
+     *     buttons: (252, 112, 43)
+     *     text: (56,50,56)
+    **/
+
+
+
+    /**
+     * Dark Theme
+     *      background:(56,50,56)
+     *      panels: (88, 74, 73)
+     *      buttons: (108, 104, 108)
+     *      text: (255, 242, 194)
+     */
+
+}

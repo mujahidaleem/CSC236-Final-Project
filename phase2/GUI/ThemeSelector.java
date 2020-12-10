6edec137805d3e9478948c1a791538a5004d0038
hannah wen <hannah.wen@mail.utoronto.ca>
2020-12-10 02:45:12 -0800

    edit MessageMenuGUI, add GUI for sending message function and theme selector

--- phase2/GUI/ThemeSelector.java
+++ phase2/GUI/ThemeSelector.java
@@ -6,24 +6,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ThemeSelector {
-    //create buttons with different themes options
-
     /**
+     *
+     * Maybe copy the below code and integrate this into the main menu?
+     * this would show up as a menu bar at the very top
      * Light theme
      *     background: (247, 245, 239)
      *     panels: (195, 226, 230)
      *     buttons: (252, 112, 43)
      *     text: (56,50,56)
-    **/
 
-
-
-    /**
      * Dark Theme
      *      background:(56,50,56)
      *      panels: (88, 74, 73)
      *      buttons: (108, 104, 108)
      *      text: (255, 242, 194)
      */
+
+    JMenuBar mb = new JMenuBar();
+    JMenu m1 = new JMenu("Theme");
+        mb.add(m1);
+    JMenuItem m11 = new JMenuItem("Light");
+    JMenuItem m22 = new JMenuItem("Dark");
+        m1.add(m11);
+        m1.add(m22);
+
+
 
 }

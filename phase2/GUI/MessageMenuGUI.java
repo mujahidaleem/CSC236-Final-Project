2c9d8742f14252c769786b389bbe69f801a8538f
hannah wen <hannah.wen@mail.utoronto.ca>
2020-12-09 19:26:20 -0800

    add MessageMenuGUI

---
+++ phase2/GUI/MessageMenuGUI.java
@@ -0,0 +1,38 @@
+package GUI;
+
+public class MessageMenuGUI {
+
+    public static void main(){
+        JFrame frame = new JFrame("Messages");
+        //Creating the Frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+
+        //Creating the MenuBar and adding components
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("Home");
+        JMenu m2 = new JMenu("logout");
+
+
+        //Creating the panel at bottom and adding components
+        JPanel panel = new JPanel(); // the panel is not visible in output
+        JLabel label = new JLabel("Enter Text");
+        JTextField tf = new JTextField(10); // accepts upto 10 characters
+        JButton send = new JButton("Send");
+        JButton reset = new JButton("Reset");
+        panel.add(label); // Components Added using Flow Layout
+        panel.add(tf);
+        panel.add(send);
+        panel.add(reset);
+
+        // Text Area at the Center
+        JTextArea ta = new JTextArea();
+
+        //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.setVisible(true);
+    }
+
+}

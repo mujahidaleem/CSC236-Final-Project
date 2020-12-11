06e4fbad1874822d20f6625d4a0b88fd1a15dbbc
hannah wen <hannah.wen@mail.utoronto.ca>
2020-12-11 14:12:41 -0800

    updated ThemeManager Colour and set cancelbutton text in UserCreationPanel

--- phase2/GUI/MainFrame/ThemeManager.java
+++ phase2/GUI/MainFrame/ThemeManager.java
@@ -22,7 +22,7 @@ public class ThemeManager {
         this.buttonColour1 = new Color(252, 132, 43);
         this.buttonColour2 = new Color(67, 165, 176);
         this.textColour = new Color(56, 50, 56);
-        this.textFieldColour = new Color(50,165,50); //TODO: Change this
+        this.textFieldColour = new Color(144, 142, 142);
     }
     /**
      * Dark Theme
@@ -37,16 +37,17 @@ public class ThemeManager {
         this.buttonColour1 = new Color(252, 132, 43);
         this.buttonColour2 = new Color(67, 165, 176);
         this.textColour = new Color(56, 50, 56);
-        this.textFieldColour = new Color(50, 165, 50); //TODO: Change this
+        this.textFieldColour = new Color(144, 142, 142); //
     }
 
     public void darkMode() {
-        this.backgroundColour = new Color(56, 50, 56);
+        this.backgroundColour = new Color(50, 50, 50);
         this.buttonColour1 = new Color(120, 60, 43);
         this.buttonColour2 = new Color(60, 90, 120);
         this.textColour = new Color(255, 242, 194);
-        this.textFieldColour = new Color(50, 165, 50); //TODO: Change this
+        this.textFieldColour = new Color(83, 83, 83); //
     }
+
 
 
     // getters

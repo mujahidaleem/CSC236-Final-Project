06ac956d5129f9f8a5c7b24198feffad9cab332e
Mujahid Aleem <mujahid.aleem@mail.utoronto.ca>
2020-12-11 23:45:26 -0500

    constructors fixed... fix the factories

--- phase2/Controllers/MessageMenu/AttendeeFriendListController.java
+++ phase2/Controllers/MessageMenu/AttendeeFriendListController.java
@@ -1,7 +1,15 @@
 package Controllers.MessageMenu;
 
 import GUI.MainMenuPanel;
+import Presenters.MessageMenu.MessageMenuPresenter;
+import UseCases.Language.LanguageManager;
+import UseCases.Message.AdminFriendManager;
 import UseCases.Message.AttendeeFriendManager;
+import UseCases.Message.OrganizerFriendManager;
+import UseCases.Message.UserFriendManager;
+import UseCases.Users.UserManager;
+
+import javax.swing.*;
 
 /**
  * Controls data specifically for attendees' friend lists.
@@ -14,7 +22,21 @@ public class AttendeeFriendListController extends UserFriendListController {
      * @param attendeeFriendManager The friend list manager use case,
      *                              holds methods relevant to manipulating attendee friend lists
      */
-    public AttendeeFriendListController(AttendeeFriendManager attendeeFriendManager, MainMenuPanel mainMenuPanel) {
-        super(attendeeFriendManager, mainMenuPanel);
+    public AttendeeFriendListController(UserFriendManager userFriendManager,
+                                        AdminFriendManager adminFriendManager,
+                                        OrganizerFriendManager organizerFriendManager,
+                                        MessageMenuPresenter messageMenuPresenter,
+                                        UserManager userManager,
+                                        JFrame frame,
+                                        LanguageManager languageManager, AttendeeFriendManager attendeeFriendManager, MainMenuPanel mainMenuPanel) {
+        super(userFriendManager,
+                adminFriendManager,
+                attendeeFriendManager,
+                organizerFriendManager,
+                mainMenuPanel,
+                messageMenuPresenter,
+                userManager,
+                frame,
+                languageManager);
     }
 }
